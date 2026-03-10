ef3d8319354eb43c7d2f0247c53b53027da2a213
Xiong Tianrui <121947202+Ihavenochoised@users.noreply.github.com>
2026-03-10 08:00:32 +0000

    Add Replit configuration files

--- replit.nix
+++ replit.nix
@@ -1,52 +1,9 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodejs-18_x
-    pkgs.chromium
-    pkgs.glib
-    pkgs.nss
-    pkgs.nspr
-    pkgs.atk
-    pkgs.at-spi2-atk
-    pkgs.cups
-    pkgs.libdrm
-    pkgs.dbus
-    pkgs.libxkbcommon
-    pkgs.mesa
-    pkgs.xorg.libXcomposite
-    pkgs.xorg.libXdamage
-    pkgs.xorg.libXext
-    pkgs.xorg.libXfixes
-    pkgs.xorg.libXrandr
-    pkgs.xorg.libX11
-    pkgs.xorg.libxcb      # Fixes your libxcb.so.1 error
-    pkgs.pango
-    pkgs.cairo
-    pkgs.expat
-    pkgs.alsa-lib
-  ];
-  env = {
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.glib
-      pkgs.nss
-      pkgs.nspr
-      pkgs.atk
-      pkgs.at-spi2-atk
-      pkgs.cups
-      pkgs.libdrm
-      pkgs.dbus
-      pkgs.libxkbcommon
-      pkgs.mesa
-      pkgs.xorg.libXcomposite
-      pkgs.xorg.libXdamage
-      pkgs.xorg.libXext
-      pkgs.xorg.libXfixes
-      pkgs.xorg.libXrandr
-      pkgs.xorg.libX11
-      pkgs.xorg.libxcb
-      pkgs.pango
-      pkgs.cairo
-      pkgs.expat
-      pkgs.alsa-lib
+{ pkgs }:
+
+{
+    deps = [
+        pkgs.zip
+        pkgs.nodejs_20
+        pkgs.chromium
     ];
-  };
 }
